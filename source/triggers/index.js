0c78e30d8b1d597b7e87e8a4e3130ba9fdfd1c6d
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-04-14 19:09:41 +0300

    unfinished tests

--- source/triggers/index.js
+++ source/triggers/index.js
@@ -1,4 +1,5 @@
 var actions = require('../actions');
+var state = {};
 
 function userRegistered(bus) {
 	bus.subscribe('user-registered', function (e) {
@@ -11,6 +12,29 @@ function userRegistered(bus) {
 
 	bus.subscribe('collection-followed', function (e) {
 		actions.notifyOwner(e);
+	});
+
+	bus.subscribe('collection-item-added', function (e) {
+		var current = state(e);
+
+		current.use(function (err, state) {
+			// log errors
+			rule(state).call(this, state);
+		});
+
+		function rule(state) {
+			return state.count > 3 ? produceAction : updateState;
+		}
+
+		function produceAction(state) {
+			current.clear(function (err) {
+				actions.notifyFollowers(e);
+			});
+		}
+
+		function updateState(state) {
+			current.update({})
+		}
 	});
 
 }
--- test/spec/server.spec.js
+++ test/spec/server.spec.js
@@ -126,5 +126,86 @@ describe('server.spec.js', function () {
 
 	describe('when collection-item-added', function () {
 
+		describe('first item added', function () {
+			beforeEach(function (done) {
+				request.post({url: url, body: {
+					event: 'collection-item-added',
+					user: 'user@notify.com',
+					data: { item: '123', collection: '345'}}, json: true}, function (err, resp) {
+						response = resp;
+						done(err);
+				});
+			});
+
+			beforeEach(function (done) {
+				setTimeout(function () {
+					utils.getLastAction(function (err, act) {
+						action = act;
+						done(err);
+					});
+				}, 30);
+			});
+
+			it('should no actions be added', function () {
+				expect(action).to.not.ok;
+			});
+		});
+
+		describe('second item added', function () {
+			beforeEach(function (done) {
+				request.post({url: url, body: {
+					event: 'collection-item-added',
+					user: 'user@notify.com',
+					data: { item: '456', collection: '345'}}, json: true}, function (err, resp) {
+						response = resp;
+						done(err);
+				});
+			});
+
+			beforeEach(function (done) {
+				setTimeout(function () {
+					utils.getLastAction(function (err, act) {
+						action = act;
+						done(err);
+					});
+				}, 30);
+			});
+
+			it('should no actions be added', function () {
+				expect(action).to.not.ok;
+			});
+		});
+
+		describe('third item added', function () {
+			beforeEach(function (done) {
+				request.post({url: url, body: {
+					event: 'collection-item-added',
+					user: 'user@notify.com',
+					data: { item: '789', collection: '345'}}, json: true}, function (err, resp) {
+						response = resp;
+						done(err);
+				});
+			});
+
+			beforeEach(function (done) {
+				setTimeout(function () {
+					utils.getLastAction(function (err, act) {
+						action = act;
+						done(err);
+					});
+				}, 30);
+			});
+
+			it('should send-notify-followers-email', function () {
+				expect(action.id).to.equal('send-notify-collection-owner-email');
+				expect(action.user).to.eql('a@a.com');
+				expect(action.collection).to.equal('345');
+				expect(action.items).to.eql(['123', '456', '789']);
+			});
+		});
+
+		describe('and repeated', function () {
+
+		});
 	});
 });
