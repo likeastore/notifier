9158a8ce781fa825296705cdf755aa6c874789ba
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-04-15 17:20:16 +0300

    all actions and triggers

--- source/actions/index.js
+++ source/actions/index.js
@@ -4,26 +4,23 @@ var db = require('../db')(config);
 function sendWelcomeEmail(e, callback) {
 	db.actions.save({
 		id: 'send-welcome',
-		email: e.data.email
+		user: e.user
 	}, callback);
 }
 
 function sendNotifyFollowersCollectionCreated(e, type, callback) {
 	db.actions.save({
 		id: 'send-notify-followers-collection-created',
-		email: e.data.followers,
-		title: e.data.title,
-		description: e.data.description,
-		collectionId: e.data.collectionId
+		user: e.user,
+		collection: e.data.collection
 	}, callback);
 }
 
 function sendNotifyOwnerCollectionFollowed(e, callback) {
 	db.actions.save({
 		id: 'send-notify-owner-collection-followed',
-		email: e.data.owner,
-		userName: e.data.followed.name,
-		userId: e.data.followed.id
+		user: e.user,
+		follower: e.data.follower
 	}, callback);
 }
 
