0e5c553a4e8923be984b2cf2c44a50f37dfb7fc4
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-04-16 10:51:22 +0300

    added param for db

--- source/db/index.js
+++ source/db/index.js
@@ -1,7 +1,8 @@
 var mongo = require('mongojs');
 
-module.exports = function (config) {
-	var db = mongo.connect(config.connection, ['events', 'actions', 'states']);
+module.exports = function (config, connection) {
+	connection = connection || 'connection';
+	var db = mongo.connect(config[connection], ['events', 'actions', 'states']);
 	if (!db) {
 		throw new Error('could not connect to ' + config.connection);
 	}
