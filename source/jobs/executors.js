5c04a45f6e7f184f7a28adc465a8647f8cd7b88e
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-04-18 13:55:20 +0300

    send-welcome executor

--- source/jobs/executors.js
+++ source/jobs/executors.js
@@ -1,10 +1,29 @@
-function sendMandrill() {
+var config = require('../../config');
+var logger = require('../utils/logger');
+var mandrill = require('node-mandrill')(config.mandrill.token);
 
+function sendMandrill(to, template, vars, callback) {
+	mandrill('/messages/send-template', {
+		template_name: template,
+		template_content: [],
+		message: {
+			auto_html: null,
+			to: to,
+			global_merge_vars: vars,
+			preserve_recipients: false
+		}
+	}, function (err) {
+		if (err) {
+			logger.error({message: 'error during mandrill send', err: err});
+		}
+
+		callback && callback(err);
+	});
 }
 
 var executors = {
 	'send-welcome': function (action, callback) {
-		callback(null);
+		sendMandrill([{email: action.data.email}], 'welcome-email', null,  callback);
 	},
 
 	'send-notify-followers-collection-created': function (action, callback) {
