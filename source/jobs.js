cc6b9a862463fa78919f8b8f2c97b2f90245999a
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-04-18 19:47:32 +0300

    purging jobs before run

--- source/jobs.js
+++ source/jobs.js
@@ -8,31 +8,31 @@ var execute = require('./jobs/execute');
 
 var agenda = new Agenda({db: {address: config.connection, collection: 'notifierJobs'} });
 
-agenda.define('resolve actions', function (job, callback) {
-	resolve(callback);
-});
-
-agenda.define('execute actions', function (job, callback) {
-	execute(callback);
-});
-
 agenda.purge(function () {
+	agenda.define('resolve actions', function (job, callback) {
+		resolve(callback);
+	});
+
+	agenda.define('execute actions', function (job, callback) {
+		execute(callback);
+	});
+
 	agenda.every('30 seconds', 'resolve actions');
 	agenda.every('1 minute', 'execute actions');
-});
 
-agenda.on('start', function (job) {
-	timing.start(job.attrs.name);
-	logger.info({message: 'job started', job: job.attrs.name });
-});
+	agenda.on('start', function (job) {
+		timing.start(job.attrs.name);
+		logger.info({message: 'job started', job: job.attrs.name });
+	});
 
-agenda.on('success', function (job) {
-	var duration = timing.finish(job.attrs.name);
-	logger.success({message: 'job compeleted', job: job.attrs.name, duration: duration.asMilliseconds()});
-});
+	agenda.on('success', function (job) {
+		var duration = timing.finish(job.attrs.name);
+		logger.success({message: 'job compeleted', job: job.attrs.name, duration: duration.asMilliseconds()});
+	});
 
-agenda.on('fail', function (err, job) {
-	logger.error({message: 'job failed', job: job.attrs.name, err: err});
-});
+	agenda.on('fail', function (err, job) {
+		logger.error({message: 'job failed', job: job.attrs.name, err: err});
+	});
 
-agenda.start();
+	agenda.start();
+});
