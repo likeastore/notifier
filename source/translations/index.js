902ca86b9856792a179d2a652f947b21202ed9bd
Guido <gvilarino@gmail.com>
2014-06-23 18:31:18 -0300

    Add translations engine

---
+++ source/translations/index.js
@@ -0,0 +1,30 @@
+/**
+ * Module dependencies.
+ */
+
+var en = require('./lib/en');
+var es = require('./lib/es');
+// var ca = require('./lib/ca');
+// var fr = require('./lib/fr');
+// var fi = require('./lib/fi');
+// var it = require('./lib/it');
+
+module.exports.help = function(t) {
+  // English
+  t.en = en;
+
+  // Spanish
+  t.es = es;
+
+  // // Català
+  // t.ca = ca;
+
+  // // French
+  // t.fr = fr;
+
+  // // Finnish
+  // t.fi = fi;
+
+  // // Italian
+  // t.it = it;
+}
