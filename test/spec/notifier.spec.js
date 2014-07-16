3c078f47edf3312bc51d46cf33cb8e47311a3783
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-07-16 11:43:20 +0300

    moved some old code

--- test/spec/notifier.spec.js
+++ test/spec/notifier.spec.js
@@ -31,4 +31,36 @@ describe('notifier.spec.js', function () {
 		});
 	});
 
+	describe('when action is initialized', function () {
+		var actionCallback;
+
+		beforeEach(function () {
+			actionCallback = sinon.spy();
+		});
+
+		beforeEach(function () {
+			notifier.action('first-event', actionCallback);
+		});
+
+		describe('and event for corresponding action triggired', function () {
+			beforeEach(function (done) {
+				var e = {
+					event: 'first-event'
+				};
+
+				request.post({url: url, body: e, json: true}, function (err, resp) {
+					response = resp;
+					done(err);
+				});
+			});
+
+			it('should respond 201 (created)', function () {
+				expect(response.statusCode).to.equal(201);
+			});
+
+			it('should call action function', function () {
+				expect(actionCallback.called).to.equal(true);
+			});
+		});
+	});
 });
