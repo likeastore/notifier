bcd51c3ac1d14991a6595e43fb6b08c26e479168
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-07-19 16:38:22 +0300

    updated example

--- example/server.js
+++ example/server.js
@@ -1,24 +1,8 @@
 var notifier = require('../source/notifier');
 
-// just for example, here it will be call to db or external service
-var asyncRequestForUser = function (userId, callback) {
-	var user = {
-		email: 'example@likeastore.com',
-		name: 'alexander.beletsky',
-		phone: '+3805554455'
-	};
-
-	process.nextTick(function () {
-		callback(null, user);
-	});
-};
-
 notifier
 	.receive('user-registered', function (e, actions, callback) {
 		actions.create('send-welcome-email', {user: e.user}, callback);
-	})
-	.receive('user-registered', function (e, actions, callback) {
-		actions.create('send-verify-sms', {user: e.user}, callback);
 	})
 	.resolve('send-welcome-email', function (a, actions, callback) {
 		asyncRequestForUser(actions.user, function (err, user) {
@@ -27,15 +11,6 @@ notifier
 			}
 
 			actions.resolved(a, {email: user.email, name: user.name}, callback);
-		});
-	})
-	.resolve('send-verify-sms', function (a, actions, callback) {
-		asyncRequestForUser(actions.user, function (err, user) {
-			if (err) {
-				return callback(err);
-			}
-
-			actions.resolved(a, {phone: user.email}, callback);
 		});
 	})
 	.execute('send-welcome-email', function (a, transport, callback) {
@@ -56,6 +31,20 @@ notifier
 				global_merge_vars: vars
 			}
 		}, callback);
+	});
+
+notifier
+	.receive('user-registered', function (e, actions, callback) {
+		actions.create('send-verify-sms', {user: e.user}, callback);
+	})
+	.resolve('send-verify-sms', function (a, actions, callback) {
+		asyncRequestForUser(actions.user, function (err, user) {
+			if (err) {
+				return callback(err);
+			}
+
+			actions.resolved(a, {phone: user.email}, callback);
+		});
 	}).
 	execute('send-verify-sms', function (a, transport, callback) {
 		// TODO: add twillio example
@@ -63,3 +52,15 @@ notifier
 	});
 
 notifier.start(process.env.NODE_PORT || 7000);
+
+function asyncRequestForUser(userId, callback) {
+	var user = {
+		email: 'example@likeastore.com',
+		name: 'alexander.beletsky',
+		phone: '+3805554455'
+	};
+
+	process.nextTick(function () {
+		callback(null, user);
+	});
+}
