2035b56d971de930ccb714495a4f2ec4e9270311
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-07-20 09:08:23 +0300

    minor changes to transport

--- source/transport.js
+++ source/transport.js
@@ -3,27 +3,25 @@ var twilio = require('twilio');
 var config = require('../config');
 
 var setupMandrill = function () {
-	if (!self.isMandrillConfigExist()) {
+	if (!validConfig()) {
 		throw new Error('missing mandrill token, please update config.transport.mandrill section');
 	}
 
 	return mandrill(config.transport.mandrill.token);
+
+	function validConfig() {
+		return config.transport.mandrill && config.transport.mandrill.token;
+	}
 };
 
 var setupTwillio = function () {
-	if (!self.isTwilioConfigExist()) {
+	if (!validConfig()) {
 		throw new Error('missing twilio account SID or auth Token, please update config.transport.twilio section');
 	}
 
 	return twilio(config.transport.twilio.accountSid, config.transport.twilio.authToken);
-};
 
-var self = {
-	isMandrillConfigExist: function () {
-		return config.transport.mandrill && config.transport.mandrill.token;
-	},
-
-	isTwilioConfigExist: function () {
+	function validConfig() {
 		return config.transport.twilio && (config.transport.twilio.accountSid && config.transport.twilio.authToken);
 	}
 };
