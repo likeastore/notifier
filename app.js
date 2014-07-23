0412024795c5fe8cd7c4dbf91b1e8cab5862c487
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-07-23 12:28:18 +0300

    collection followed

--- app.js
+++ app.js
@@ -177,6 +177,71 @@ notifier
 
 // collection followed email
 
+notifier
+	.receive('collection-followed', function (e, actions, callback) {
+		actions.create('send-notify-owner-collection-followed', {
+			user: e.user,
+			follower: e.data.follower,
+			collection: e.data.collection,
+		}, callback);
+	})
+	.resolve('send-notify-owner-collection-followed', function (action, actions, callback) {
+		db.collections.findOne({_id: new mongo.ObjectId(action.collection)}, function (err, collection) {
+			if (err) {
+				return callback(err);
+			}
+
+			if (!collection) {
+				return callback({message: 'collection not found', collection: action.collection});
+			}
+
+			db.users.findOne({_id: new mongo.ObjectId(action.follower)}, function (err, follower) {
+				if (err) {
+					return callback(err);
+				}
+
+				if (!follower) {
+					return callback({message: 'user not found', email: action.follower});
+				}
+
+				var data = {
+					email: collection.userData.email,
+					user: _.pick(collection.userData, userPick),
+					follower: follower,
+					collection: _.pick(collection, collectionPick)
+				};
+
+				actions.resolved(action, data, callback);
+			});
+		});
+	})
+	.execute('send-notify-owner-collection-followed', function (action, transport, callback) {
+		var follower = action.data.follower;
+		var user = action.data.user;
+		var collection = action.data.collection;
+
+		var vars = [
+			{ name: 'USERID', content: action.data.user._id },
+			{ name: 'USER_NAME', content: follower.name },
+			{ name: 'USER_AVATAR', content: follower.avatar },
+			{ name: 'COLLECTION_URL', content: formatUrl(user, collection) },
+			{ name: 'COLLECTION_TITLE', content: collection.title },
+			{ name: 'COLLECTION_THUMBNAIL', content: collection.thumbnail }
+		];
+
+		transport.mandrill('/messages/send-template', {
+			template_name: 'notify-owner-collection-followed',
+			template_content: [],
+			message: {
+				auto_html: null,
+				to: [{email: action.data.email}],
+				bcc: 'ceo@likeastore.com',
+				global_merge_vars: vars,
+				preserve_recipients: false
+			}
+		}, callback);
+	});
+
 // user feedback (email to developers)
 
 // sorry see you go email
