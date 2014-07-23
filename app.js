590cf591524f8f7ae52b411026a5704e14df570f
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-07-23 11:54:40 +0300

    personal email

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var _ = require('underscore');
 var mongo = require('mongojs');
+var moment = require('moment');
 
 var config = require('./config');
 var notifier = require('./source/notifier');
@@ -53,6 +54,49 @@ notifier
 	});
 
 // into email
+
+notifier
+	.receive('user-registered', function (e, actions, callback) {
+		actions.create('send-personal', {user: e.user, executeAfter: moment().add(3, 'days').toDate()}, callback);
+	})
+	.resolve('send-personal', function (action, actions, callback) {
+		db.users.findOne({email: action.user}, function (err, user) {
+			if (err) {
+				return callback(err);
+			}
+
+			if (!user) {
+				return callback({message: 'user not found', email: action.email});
+			}
+
+			var data = {
+				email: action.user,
+				user: _.pick(user, userPick)
+			};
+
+			actions.resolved(action, data, callback);
+		});
+	})
+	.execute('send-personal', function (action, transport, callback) {
+		var user = action.data.user;
+
+		var vars = [
+			{ name: 'USER_NAME', content: user.displayName || user.name },
+			{ name: 'USER_EMAIL', content: user.email },
+		];
+
+		transport.mandrill('/messages/send-template', {
+			template_name: 'personal-email',
+			template_content: [],
+			message: {
+				auto_html: null,
+				to: [{email: action.data.email}],
+				bcc: 'ceo@likeastore.com',
+				global_merge_vars: vars,
+				preserve_recipients: false
+			}
+		}, callback);
+	});
 
 // collection created email
 
