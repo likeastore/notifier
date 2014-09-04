8bea27cc4c2d96cd2f6dd1b5fb1460844875b9c9
juliausti <julian.ustiyanovych@gmail.com>
2014-09-05 00:23:10 +0300

    Add rest hook implementation

--- source/transport.js
+++ source/transport.js
@@ -50,10 +50,16 @@ var setupAndroidPushNotification = function () {
 		}
 
 		var service = new gcm.Sender(config.transport.gcm.serverApiKey);
-		var message = new gcm.Message();
+		var message = new gcm.Message({
+			collapseKey: 'notifier',
+			timeToLive: 3600 * 2,
+			delayWhileIdle: false
+		});
 
 		message.addDataWithObject(options.message);
-		service.send(message, options.tokens, options.retries, callback);
+
+
+		service.send(message, options.regIds, options.retries, callback);
 
 		function validOptions() {
 			return options.message && options.regIds && options.retries;
@@ -87,7 +93,7 @@ var setupIOSPushNotification = function () {
 			developmentGateway = 'gateway.sandbox.push.apple.com';
 
 		if(!validOptions()) {
-			var errorMsg = "missing 'options' or required , please make sure you have provided";
+			var errorMsg = "missing 'options' or required fields, please make sure you have that options are defined";
 			logger.error(errorMsg);
 			throw new Error(errorMsg);
 		}
@@ -98,7 +104,7 @@ var setupIOSPushNotification = function () {
 		service.pushNotification(note, options.tokens);
 
 		function validOptions() {
-			return options && options.alert &&  options.production && (options.tokens && options.tokens.length > 0);
+			return options && options.alert && (options.tokens && options.tokens.length > 0);
 		}
 
 		function initConnection() {
