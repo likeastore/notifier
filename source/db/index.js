8c05c62189d4e2feece2cc90c975165dbcece7e0
fuad <fuad@infosun.co.uk>
2016-08-20 05:37:53 +0500

    Modernization of Notifier Module, updated all dependencies to latest.

--- source/db/index.js
+++ source/db/index.js
@@ -2,7 +2,7 @@ var mongo = require('mongojs');
 
 module.exports = function (config, connection) {
 	connection = connection || 'connection';
-	var db = mongo.connect(config[connection], ['actions']);
+	var db = mongo(config[connection], ['actions']);
 	if (!db) {
 		throw new Error('could not connect to ' + config.connection);
 	}
